4511e87c58795cb7131017199eda1b333bafd824
dily38250022163.com <dily38250022163.com>
2014-11-24 19:19:56 +0800

    修改backFun注意事项

--- src/core/component/ImgButton.ts
+++ src/core/component/ImgButton.ts
@@ -17,7 +17,7 @@ class ImgButton extends egret.DisplayObjectContainer{
     private cartoonType:number = 1;
     /**
     * imgName       图片
-    * backFun       点击方法
+    * backFun       点击方法 如果需要在backFun中使用this的，小心使用这个
     * descStr       按钮描述
     * fontSize      字体大小
     * cartoonType   动画类型 1:【可爱】按下变小，放开弹大 2:按下变小，放开轻微弹大 3：按下变小，放开变大
