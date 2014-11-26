0297b42c356fb13a22e3162e0afc06b6c93a20a1
dily38250022163.com <dily38250022163.com>
2014-11-26 16:18:38 +0800

    修改tips特效bug

--- src/core/utils/EffectUtils.ts
+++ src/core/utils/EffectUtils.ts
@@ -1,19 +1,19 @@
   /**
-	* 游戏特效汇总
-	* by zhaoxin
-	* (c) copyright 2014 - 2035
-	* All Rights Reserved. 
-	* 使用方法如：EffectUtils.rotationEffect()
+    * 游戏特效汇总
+    * by zhaoxin
+    * (c) copyright 2014 - 2035
+    * All Rights Reserved. 
+    * 使用方法如：EffectUtils.rotationEffect()
     */
 
 module EffectUtils {
 
     // 存储旋转对象
     var rotationArr:Array<any> = [];
-	//对象旋转特效
+    //对象旋转特效
     //obj   旋转对象
     //time  旋转一周用时，毫秒
-	export function rotationEffect(obj,time:number = 1000):void{
+    export function rotationEffect(obj,time:number = 1000):void{
         if(this.rotationArr == null){
             this.rotationArr = [];
         }
@@ -24,14 +24,14 @@ module EffectUtils {
             this.rotationArr[obj.hashCode] = true;
         }
         var onComplete1:Function = function(){
-        	if(this.rotationArr[obj.hashCode]&&(obj != null)){
-	        	obj.rotation = 0;
-	            egret.Tween.get(obj).to({rotation:360},time).call(onComplete1,this);   
-        	}
+            if(this.rotationArr[obj.hashCode]&&(obj != null)){
+                obj.rotation = 0;
+                egret.Tween.get(obj).to({rotation:360},time).call(onComplete1,this);   
+            }
         };
         obj.rotation = 0;
-		egret.Tween.get(obj).to({rotation:360},time).call(onComplete1,this);   
-	}
+        egret.Tween.get(obj).to({rotation:360},time).call(onComplete1,this);   
+    }
 
     //取消对象旋转
     //obj    旋转对象
@@ -39,33 +39,33 @@ module EffectUtils {
           this.rotationArr[obj.hashCode] = false;
     }
 
-	//对象闪烁特效
+    //对象闪烁特效
     //obj         闪烁对象
     //interval    闪烁总工时间
-	export function blinkEffect(obj,interval:number):void{
+    export function blinkEffect(obj,interval:number = 1000):void{
         new BitmapBlink(obj,interval); 
-	}
+    }
 
-	//抖动对象特效
+    //抖动对象特效
     //类似ios密码输入错误的特效
-	export function shakeObj(obj):void{
-		var shakeNum = 80;
-		var oldX:number = obj.x;
-		egret.Tween.get(obj).to({x:obj.x - 10},shakeNum); 
+    export function shakeObj(obj):void{
+        var shakeNum = 80;
+        var oldX:number = obj.x;
+        egret.Tween.get(obj).to({x:obj.x - 10},shakeNum); 
 
         egret.setTimeout(function () {              
-			egret.Tween.get(obj).to({x:obj.x + 20},shakeNum); 
+            egret.Tween.get(obj).to({x:obj.x + 20},shakeNum); 
         }, this, shakeNum*2); 
         egret.setTimeout(function () {              
-			egret.Tween.get(obj).to({x:obj.x - 20},shakeNum); 
+            egret.Tween.get(obj).to({x:obj.x - 20},shakeNum); 
         }, this, shakeNum*3); 
         egret.setTimeout(function () {              
-			egret.Tween.get(obj).to({x:obj.x + 20},shakeNum); 
+            egret.Tween.get(obj).to({x:obj.x + 20},shakeNum); 
         }, this, shakeNum*4); 
         egret.setTimeout(function () {              
-			egret.Tween.get(obj).to({x:oldX},shakeNum); 
-        }, this, shakeNum*5); 			
-	}
+            egret.Tween.get(obj).to({x:oldX},shakeNum); 
+        }, this, shakeNum*5);           
+    }
 
 
     //抖动对象特效
@@ -114,7 +114,7 @@ module EffectUtils {
 
     /**
     * str             提示内容
-    * effectType      动画类型 0：从下到上弹出 1：从左至右弹出 2：从右至左弹出 3：从中间弹出渐渐消失 4：从大变小 5：从小变大 等等
+    * effectType      动画类型 1：从下到上弹出 2：从左至右弹出 3：从右至左弹出 4：从中间弹出渐渐消失 5：从大变小 等等
     * isWarning       是否是警告，警告是红色
     */
     export function showTips(str:string = "",effectType:number = 1,isWarning:boolean = false):void{
